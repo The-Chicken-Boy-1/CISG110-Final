f51d837de49bd31bf718c22d96cbb469a6022c64
The-Chicken-Boy-1 <loganmaravilla999@gmail.com>
2026-05-05 22:08:24 -0700

    teddy explosive when it hits something

--- cisg-110-final/scenes/scripts/player.gd
+++ cisg-110-final/scenes/scripts/player.gd
@@ -1,9 +1,21 @@
 extends CharacterBody2D
 
+@export var SPEED = 300.0
+@export var JUMP_VELOCITY = -400.0
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var _maxKickTime: float = 0.5
+var _kickTimer: float = 0.0
 
+@export var _kickRight: Node2D
+@export var _kickLeft: Node2D
+
+@export var _rightKickDir: Vector2 = Vector2.RIGHT
+@export var _leftKickDir: Vector2 = Vector2i.LEFT
+
+var _facingRight: bool = true
+
+func _enter_tree() -> void:
+	_disableKick()
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,7 +23,7 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
@@ -21,5 +33,44 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	if direction > 0:
+		_facingRight = true
+	elif direction < 0:
+		_facingRight = false
+	if _kickTimer > 0:
+		_kickTimer -= delta
+		
+		if _kickTimer <= 0:
+			_disableKick()
 
+	if Input.is_action_just_pressed("ui_accept"):
+		_kick()
+	
 	move_and_slide()
+
+func _kick() -> void:
+	if _facingRight:
+		_kickRight.process_mode = Node.PROCESS_MODE_INHERIT
+	else:
+		_kickLeft.process_mode = Node.PROCESS_MODE_INHERIT
+	_kickTimer = _maxKickTime
+
+func _disableKick() -> void:
+	_kickRight.process_mode = Node.PROCESS_MODE_DISABLED
+	_kickLeft.process_mode = Node.PROCESS_MODE_DISABLED
+
+func _on_kick_right_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	print(body.name)
+	body.apply_central_impulse(_rightKickDir)
+	
+	if body is Prop:
+		body._was_kicked = true
+
+
+func _on_kick_left_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	print(body.name)
+	body.apply_central_impulse(_leftKickDir)
+	
+	if body is Prop:
+		body._was_kicked = true
